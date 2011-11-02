b40e4d3dc2c3295cffa8fda62ca67ae747d73c5f
benibela <benito@benibela.de>
2011-11-02 12:05:06 +0100

    x11 watcher
    --HG--
    branch : trunks

---
+++ watcher.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+TMPFILE=/ramdisk/curwatch
+RESFILE=`date  +"/home/benito/hg/programs/system/watcher/records/%F_%T"`
+LOOPS=30
+#10*60/20
+
+
+if [ -f $RESFILE ]; then echo $RESFILE exists;  exit; fi
+
+rm -f $TMPFILE
+
+STOP=
+trap "STOP=true" SIGTERM
+
+i=0;
+
+while [[ -z $STOP ]]; do
+
+ID=`xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)" | cut -d ' ' -f 5`
+TITLE=`xwininfo -id $ID | grep "Window id"  | cut -d "\"" -f 2`
+PROCID=`xwininfo -id $ID -all | grep "Process id"  | grep -oE [0-9]+ `
+PROG=`ps p$PROCID | tail -1 | tr -s " " "#"  | cut -d "#" -f 6-10  --output-delimiter " " | tr : " "`
+DATE=`date  +"%F %T"`
+
+echo $DATE: $PROG: $TITLE >> $TMPFILE
+
+sleep 20;
+
+((i++));
+
+#echo ping
+
+if [[ $i -gt $LOOPS ]]; then
+#echo crypted
+gpg -e -r benito_local --output $RESFILE.TMP < $TMPFILE
+mv $RESFILE.TMP $RESFILE
+i=0
+fi
+
+done
+
+gpg -e -r benito_local --output $RESFILE.TMP < $TMPFILE
+mv $RESFILE.TMP $RESFILE
+
